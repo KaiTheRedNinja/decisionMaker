318c3835318a60686a55b7d8cd865a2bc989de05
KaiTheRedNinja <88234730+KaiTheRedNinja@users.noreply.github.com>
2023-01-14 18:07:12 +0800

    Add coin view

--- decisionMaker/Randomization/RandomView.swift
+++ decisionMaker/Randomization/RandomView.swift
@@ -8,8 +8,30 @@
 import SwiftUI
 
 struct RandomView: View {
+    @State var selection: String = "coin"
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Spacer()
+            switch selection {
+            case "coin":
+                CoinView()
+            default:
+                Text("You've gone too far")
+            }
+            Spacer()
+        }
+        .safeAreaInset(edge: .top) {
+            Picker(selection: $selection) {
+                ForEach(["coin", "dice", "8ball"], id: \.self) { name in
+                    Text(name)
+                }
+            } label: {
+                EmptyView()
+            }
+            .pickerStyle(.segmented)
+            .padding(.horizontal, 10)
+        }
     }
 }
 
