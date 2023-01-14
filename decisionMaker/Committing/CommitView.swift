5845a40a2dd22852eef9efb21d7ba4626051f698
KaiTheRedNinja <88234730+KaiTheRedNinja@users.noreply.github.com>
2023-01-14 16:10:50 +0800

    just do it

--- decisionMaker/Committing/CommitView.swift
+++ decisionMaker/Committing/CommitView.swift
@@ -8,8 +8,122 @@
 import SwiftUI
 
 struct CommitView: View {
+    @State var goingToDo: Bool?
+    @State var percentTimeLeft: CGFloat = 1
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            if goingToDo != nil {
+                doOrDoNotView
+            } else {
+                willYouDoItView
+            }
+        }
+    }
+
+    var willYouDoItView: some View {
+        VStack {
+            Text("Are you going to do it?")
+                .font(.title)
+                .padding(.bottom, 30)
+            HStack {
+                Spacer()
+                Button {
+                    goingToDo = true
+                } label: {
+                    Text("YES")
+                        .font(.title)
+                        .foregroundColor(.init(uiColor: UIColor.systemBackground))
+                        .frame(width: 140, height: 100)
+                        .background {
+                            Color.green
+                                .cornerRadius(15)
+                        }
+                }
+                Spacer()
+                Button {
+                    goingToDo = false
+                } label: {
+                    Text("NO")
+                        .font(.title)
+                        .foregroundColor(.init(uiColor: UIColor.systemBackground))
+                        .frame(width: 140, height: 100)
+                        .background {
+                            Color.red
+                                .cornerRadius(15)
+                        }
+                }
+                Spacer()
+            }
+        }
+    }
+
+    var doItView: some View {
+        ZStack {
+            Color.green
+                .opacity(0.5)
+                .ignoresSafeArea()
+            Text("GO DO IT!")
+                .font(.title)
+                .bold()
+        }
+    }
+
+    var doOrDoNotView: some View {
+        ZStack {
+            if goingToDo ?? true {
+                Color.green
+                    .opacity(0.5)
+                    .ignoresSafeArea()
+                Text("GO DO IT!")
+            } else {
+                Color.red
+                    .opacity(0.5)
+                    .ignoresSafeArea()
+                Text("DONT DO IT!")
+            }
+        }
+        .font(.title)
+        .bold()
+        .safeAreaInset(edge: .bottom) {
+            Button {
+                withAnimation {
+                    goingToDo = nil
+                }
+            } label: {
+                Text("Return")
+                    .foregroundColor(.primary)
+                    .padding(10)
+                    .background {
+                        GeometryReader { proxy in
+                            if goingToDo ?? true {
+                                Color.green
+                                    .cornerRadius(5)
+                                    .frame(width: proxy.size.width*percentTimeLeft)
+                            } else {
+                                Color.red
+                                    .cornerRadius(5)
+                                    .frame(width: proxy.size.width*percentTimeLeft)
+                            }
+                        }
+                    }
+                    .onAppear {
+                        DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
+                            withAnimation(.linear(duration: 5)) {
+                                percentTimeLeft = 0
+                            }
+                            DispatchQueue.main.asyncAfter(deadline: .now() + 5) {
+                                withAnimation {
+                                    goingToDo = nil
+                                }
+                                DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+                                    percentTimeLeft = 1
+                                }
+                            }
+                        }
+                    }
+            }
+        }
     }
 }
 
