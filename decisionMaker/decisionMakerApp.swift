b3b5587914a5c8e176ec1589faca046d841bf442
KaiTheRedNinja <88234730+KaiTheRedNinja@users.noreply.github.com>
2023-01-14 13:24:15 +0800

    Initial commit

---
+++ decisionMaker/decisionMakerApp.swift
@@ -0,0 +1,17 @@
+//
+//  decisionMakerApp.swift
+//  decisionMaker
+//
+//  Created by Kai Quan Tay on 14/1/23.
+//
+
+import SwiftUI
+
+@main
+struct decisionMakerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
